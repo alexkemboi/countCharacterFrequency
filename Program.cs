27ea23cc2c4dd65719e805b15b0eccc4900cbafd
ALEX K. KEMBOI <AKEMBOI@fintech-group.com>
2023-06-20 15:25:49 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = "Hello, World!";
+        Hashtable charFrequency = CountCharacterFrequency(input);
+
+        foreach (DictionaryEntry entry in charFrequency)
+        {
+            char character = (char)entry.Key;
+            int frequency = (int)entry.Value;
+            Console.WriteLine($"Character: '{character}', Frequency: {frequency}");
+        }
+    }
+
+    static Hashtable CountCharacterFrequency(string input)
+    {
+        Hashtable charFrequency = new Hashtable();
+
+        foreach (char c in input)
+        {
+            if (charFrequency.ContainsKey(c))
+            {
+                int frequency = (int)charFrequency[c];
+                charFrequency[c] = frequency + 1;
+            }
+            else
+            {
+                charFrequency.Add(c, 1);
+            }
+        }
+
+        return charFrequency;
+    }
+}
